944279fd2fcc44951c09d3a8f6d78032db169db0
bjwells <brandonj@wells@hotmail.ca>
2017-03-25 15:46:57 -0300

    inital commit

---
+++ src/sample/OpenController.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import javax.swing.*;
+import javafx.event.ActionEvent;
+
+public class OpenController {
+
+    @FXML
+    private void handleOpenButtonAction(ActionEvent event)
+    {
+        //button was clicked do something
+        //JOptionPane.showMessageDialog(null, "clicked open button");
+    }
+
+    @FXML
+    private void handleNewAttributeButtonAction(ActionEvent event)
+    {
+        //button was clicked
+    }
+
+    @FXML
+    private void handleNewDefaultButtonAction(ActionEvent event)
+    {
+
+    }
+}
